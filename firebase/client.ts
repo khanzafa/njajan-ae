7f94eecae25864063527462d8414c1e9eb041a23
khanzafa <khanzafadilaazz@gmail.com>
2024-05-24 01:15:30 +0700

    fix deploy error

--- firebase/client.ts
+++ firebase/client.ts
@@ -12,12 +12,12 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_FIREBASE_APIKEY,
-  authDomain: process.env.NEXT_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.NEXT_FIREBASE_PROJECTID,
-  storageBucket: process.env.NEXT_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_FIREBASE_SENDERID,
-  appId: process.env.NEXT_FIREBASE_APPID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
